0b26d474eeccce3bf865ef386f1fbedc37418c23
lavishakaushik24-git <lavishakaushik24@gmail.com>
2026-07-29 16:14:17 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+class Assignment1
+{
+    public static void main(String[]args){
+        System.out.println("This is my first java program");
+        System.out.println("This is session one");
+        System.out.println("Hello World");
+    }
+}
